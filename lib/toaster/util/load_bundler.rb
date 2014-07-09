4b22813ae15f7677f96a5d7eb30df21bfc05ae8e
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2014-07-09 18:40:14 +0000

    fix load path

--- lib/toaster/util/load_bundler.rb
+++ lib/toaster/util/load_bundler.rb
@@ -2,7 +2,7 @@
 # invoke all necessary *require*s using Bundler
 
 if !$toaster_bundler_loaded
-  TOASTER_ROOT_DIR = File.join(File.dirname(__FILE__), "..","..","..","..")
+  TOASTER_ROOT_DIR = File.join(File.dirname(__FILE__), "..","..","..")
   ENV['BUNDLE_GEMFILE'] = File.join(TOASTER_ROOT_DIR, "Gemfile")
   require 'rubygems'
   require 'bundler/setup'
