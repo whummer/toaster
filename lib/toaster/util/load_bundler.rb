407ef6caa958fc4f1a3180a01c520d98a4f58d6c
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2014-07-09 19:21:35 +0000

    fix LOAD_PATH, suppress warnings

--- lib/toaster/util/load_bundler.rb
+++ lib/toaster/util/load_bundler.rb
@@ -6,10 +6,18 @@ if !$toaster_bundler_loaded
   ENV['BUNDLE_GEMFILE'] = File.join(TOASTER_ROOT_DIR, "Gemfile")
   require 'rubygems'
   require 'bundler/setup'
+
   # bug fix for ruby 1.9+ required to load gem tidy
+  original_verbosity = $VERBOSE
+  $VERBOSE = nil # disable warning "DL is deprecated, please use Fiddle"
   require 'dl/import'
   DL::Importable = DL::Importer
+  $VERBOSE = original_verbosity
+
   # call bundler
   Bundler.require(:default)
+
+  # $LOAD_PATH changed by Bundler, we have to add toaster again
+  $LOAD_PATH << File.join(TOASTER_ROOT_DIR, "lib")
   $toaster_bundler_loaded = true
 end
