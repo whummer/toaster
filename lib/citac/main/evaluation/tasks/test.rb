34769208096772eb1eb43df2fd94bae4aad18887
Oliver Hanappi <oliver.hanappi@gmail.com>
2015-08-27 14:31:18 +0200

    Implemented that test cases are skipped after five aborts

--- lib/citac/main/evaluation/tasks/test.rb
+++ lib/citac/main/evaluation/tasks/test.rb
@@ -19,14 +19,20 @@ module Citac
           false
         end
 
+        def unknown_test_case_exists?(suite, suite_result)
+          suite.test_cases.any? { |tc| suite_result.overall_case_result(tc) == :unknown }
+        end
+
         def get_pending_test_case(suite, suite_result)
-          suite.test_cases.find{|tc| suite_result.overall_case_result(tc) == :unknown}
+          suite.test_cases.find{ |tc| suite_result.overall_case_result(tc) == :unknown &&
+              (suite_result.test_case_results[tc.id] || []).size < 5} # skip test case after 5 aborts
         end
 
         def fulfilled?(spec, operating_system)
           suite = get_suite spec, operating_system
           suite_result = spec_repository.test_suite_results spec, operating_system, suite
-          get_pending_test_case(suite, suite_result).nil?
+
+          get_pending_test_case(suite, suite_result).nil? && !unknown_test_case_exists?(suite, suite_result)
         rescue
           return false
         end
@@ -36,14 +42,11 @@ module Citac
           suite_result = spec_repository.test_suite_results spec, operating_system, suite
 
           pending_test_case = get_pending_test_case suite, suite_result
-          return :success_completed unless pending_test_case
+          return (unknown_test_case_exists?(suite, suite_result) ? :failure : :success_completed) unless pending_test_case
 
           args = ['test', 'exec', spec.id, operating_system, suite.id, pending_test_case.id]
           result = Citac::Utils::Exec.run 'citac', :args => args, :output => :passthrough, :raise_on_failure => false
-          return :failure if result.failure?
-
-          suite_result = spec_repository.test_suite_results spec, operating_system, suite
-          (get_pending_test_case suite, suite_result).nil? ? :success_completed : :success_partial
+          result.success? ? :success_partial : :failure
         end
       end
     end
